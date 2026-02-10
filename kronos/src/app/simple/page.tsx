62447ada81635e053e1bbbf35836a3d7ff4d009c
Joao Kronos <joao@kronosync.com>
2026-02-10 14:22:28 -0300

    debug: add static simple page to isolate 500 error

---
+++ kronos/src/app/simple/page.tsx
@@ -0,0 +1,8 @@
+export default function SimplePage() {
+    return (
+        <div style={{ backgroundColor: 'black', color: 'white', padding: '50px' }}>
+            <h1>STATIC TEST PAGE</h1>
+            <p>If you can see this, the Root Layout is not broken, and the middleware is not the cause of the 500.</p>
+        </div>
+    )
+}
