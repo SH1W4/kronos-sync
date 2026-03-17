3a6d8ebb8f1aac7dacba7d32fbd4ecf15c85f990
Joao Kronos <joao@kronosync.com>
2026-03-17 10:37:06 -0300

    debug: simplified Providers.tsx to isolate SSR crash

--- kronos/src/components/providers.tsx
+++ kronos/src/components/providers.tsx
@@ -1,14 +1,7 @@
 'use client'
 
 import React from 'react'
-import { ThemeProvider } from "@/contexts/theme-context"
-import { Toaster } from "@/components/ui/toaster"
 
 export function Providers({ children }: { children: React.ReactNode }) {
-    return (
-        <ThemeProvider>
-            {children}
-            <Toaster />
-        </ThemeProvider>
-    )
+  return <>{children}</>
 }
