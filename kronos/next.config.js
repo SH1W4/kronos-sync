f44e2bd5a806fa9fef37f835cf2549e714e0889f
Joao Kronos <joao@kronosync.com>
2026-03-18 01:09:09 -0300

    fix(config): hardcode NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY to bypass vercel env missing issue

--- kronos/next.config.js
+++ kronos/next.config.js
@@ -10,6 +10,9 @@ const nextConfig = {
     },
     experimental: {
     },
+    env: {
+        NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY: process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY || "pk_test_c3VtbWFyeS1zdG9yay00OS5jbGVyay5hY2NvdW50cy5kZXYk",
+    }
 }
 
 module.exports = nextConfig
